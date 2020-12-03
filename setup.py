149665515db4d62e6f2c1b6ed9dd242fb5311c9c
Arslan Abdizhalilov <arslan.abdizhalilov@gmail.com>
2020-12-03 02:36:11 +0100

    Add feh to arch package list

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def run(command):
     return subprocess.call(command, shell=True)
 
 
-arch_packages = "firefox telegram-desktop discord freeoffice microsoft-office-web-jak vlc qbittorrent nautilus python-nautilus grub-customizer gucharmap tilix zsh rofi timeset blueman pulseaudio-bluetooth pa-applet pavucontrol neofetch speedtest-cli wmctrl xclip redshift noto-fonts-emoji"
+arch_packages = "firefox telegram-desktop discord freeoffice microsoft-office-web-jak vlc qbittorrent nautilus python-nautilus grub-customizer gucharmap tilix zsh rofi feh timeset blueman pulseaudio-bluetooth pa-applet pavucontrol neofetch speedtest-cli wmctrl xclip redshift noto-fonts-emoji"
 aur_packages = "google-chrome-stable spotify spicetify-cli spotify-tui spotifyd  mailspring teams nautilus-empty-file visual-studio-code-bin clion pycharm-professional intellij-idea-ultimate-edition webstorm datagrip i3-battery-popup-git"
 vscode_extensions = ["James-Yu.latex-workshop", "fallenwood.vimL", "icrawl.discord-vscode", "ms-python.python", "ms-vscode.cpptools", "ms-vsliveshare.vsliveshare", "ritwickdey.LiveServer", "vscode-icons-team.vscode-icons", "j-james.adapta-nokto-vscode"]
 
